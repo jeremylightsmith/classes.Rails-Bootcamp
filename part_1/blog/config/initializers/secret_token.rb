ffb09d6430cff3fea51748636e784d53a4855f3c
Wes Maldonado <wes@brokenbuild.com>
2010-10-31 09:46:30 -0700

    Part 1 updated for Rails 3

---
+++ part_1/blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'f975b641be94ee55b5163f3d057c65830427e7880a79420ea7b3bcea0ec095dfb4e21b8470c98e43e7fb6e41be35672d6613211bed17f66ed1887099620ce0d8'
